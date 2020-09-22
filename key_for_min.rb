0d69fbf677ef1e230cd3feadfb480910518bcf90
Alie Stone <helloaliestone@gmail.com>
2020-09-22 21:40:37 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,5 @@
 def key_for_min_value(name_hash)
   return nil if hash.empty?
   arr = hash.collect { |k, v| v }.sort
-end
-hash.each { |k, v| return k if v == arr[0] }
+  hash.each { |k, v| return k if v == arr[0] }
 end
